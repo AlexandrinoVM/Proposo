de98f611a62418eba4fe7b790f30dab83d4ff16d
AlexandrinoVM <vinicius.moraes2@outlook.com>
2025-07-06 10:13:02 -0300

    adding contract route

--- src/controllers/contract.controller.ts
+++ src/controllers/contract.controller.ts
@@ -1,14 +1,17 @@
 import { ContractData } from "../models/contract.model.js";
 import { ContractService } from "../services/contract.service.js";
+import { Request, Response } from 'express';
 
-
-export class ContractController{
-    static async generate(data:ContractData){
+export class ContractController {
+    static async generate(req:Request,res:Response){
+        const data:ContractData = req.body;
         try{
             const pathToPdf = await ContractService.createContract(data);
             console.log(`pdf generated with sucess  in: ${pathToPdf}`)
+            return res.status(201).json({sucess: true,message:'contract created with sucess',path:pathToPdf})
         }catch(err){
             console.log(`could not generate contract:${err}`)
+            return res.status(500).json({sucess: false,message:'contract not created',error:err})
         }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,32 @@
 import { pool } from './config/database.js'
-
+import express from 'express'
+import {Router,Request,Response} from 'express'
 import { ContractController } from './controllers/contract.controller.js';
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express()
 
-ContractController.generate({
-  clientName: 'João da Silva',
-  serviceName: 'Desenvolvimento Web',
-  value: 2500,
-  dueDate: '2025-07-15'
-});
+const route = Router()
 
+app.use(express.json())
+
+
+route.post('/',(req:Request,res:Response)=>{
+    ContractController.generate(req,res);
+})
+
+// ContractController.generate({
+//   clientName: 'João da Silva',
+//   serviceName: 'Desenvolvimento Web',
+//   value: 2500,
+//   dueDate: '2025-07-15'
+// });
+
+
+app.use(route)
+const port = process.env.PORT || 3333
+
+app.listen(port,()=>console.log( `server running on port ${port}`))
 
 
 // async function start() {
